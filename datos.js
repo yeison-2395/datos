7c2a3358ccf301c884a1bdc534bb66f3895aa371
yeison-2395 <yac2395.ya@gmail.com>
2024-07-31 19:33:06 -0500

    codigo - actualizado

---
+++ datos.js
@@ -0,0 +1,11 @@
+
+let nombre = prompt("Por favor, ingresa tu nombre.")
+if (nombre) {
+document.write("su nombre es" + nombre)
+};
+
+let edad = prompt(parseInt('ingrese su edad'))
+if (edad) {
+document.write('su edad es: ' + edad);
+}
+
